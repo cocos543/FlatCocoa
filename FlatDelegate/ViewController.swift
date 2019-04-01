36df8701fa9bfb035643ca607063c701c70a1d10
Cocos <543314602@qq.com>
2019-04-01 16:43:14 +0800

    Add project.

---
+++ FlatDelegate/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FlatDelegate
+//
+//  Created by Cocos on 2019/3/19.
+//  Copyright © 2019年 Cocos. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import FlatCocoa
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        FlatMain().sayHello()
+
+    }
+}
+
